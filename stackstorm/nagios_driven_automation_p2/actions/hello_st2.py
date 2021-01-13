b66c07219744477f0acc023e3c61bc0f5eb5a48d
Krishna Kadiyala <k.kadiyala@tcu.edu>
2021-01-13 07:23:40 -0600

    Pythong for Network Engineers
    Forked from Kirk Byers Free Python course.

--- stackstorm/nagios_driven_automation_p2/actions/hello_st2.py
+++
@@ -1,7 +0,0 @@
-from st2common.runners.base_action import Action
-
-
-class HelloStackStorm(Action):
-    def run(self):
-        print("Hello Stackstorm, want to be friends?")
-        return True, "Let's be friends!"
